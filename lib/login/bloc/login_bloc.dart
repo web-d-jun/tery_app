79cabf6b29f1982e84f5804ae3bc71bb51b1e654
junyoung <cjy874545@gmail.com>
2022-03-12 22:42:21 +0900

    id 입력

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
+import 'package:tery_app/login/login.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -14,6 +16,13 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final AuthenticationRepository _authenticationRepository;
 
   void _onUsernameChanged(LoginUsernameChanged event, Emitter<LoginState> emit) {
-    print('$event ......');
+    print('${event.username} ......');
+    final username = Username.dirty(event.username);
+    print('$username this username');
+    print(state.password);
+    emit(state.copyWith(
+      username: username,
+      status: Formz.validate([state.password, username]),
+    ));
   }
 }
